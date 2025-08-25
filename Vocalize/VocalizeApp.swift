1299da42d2e82d6c4275220aabdc74bd1ddbd35c
AntonioPaess <paesdeandradeantonio@gmail.com>
2025-08-25 11:42:33 -0300

    Initial Commit

---
+++ Vocalize/VocalizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocalizeApp.swift
+//  Vocalize
+//
+//  Created by Antônio Paes De Andrade on 25/08/25.
+//
+
+import SwiftUI
+
+@main
+struct VocalizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VocalizeTests/VocalizeTests.swift
@@ -0,0 +1,17 @@
+//
+//  VocalizeTests.swift
+//  VocalizeTests
+//
+//  Created by Antônio Paes De Andrade on 25/08/25.
+//
+
+import Testing
+@testable import Vocalize
+
+struct VocalizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
